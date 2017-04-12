f85b0df5c5d21a3b82a3de456bb0b4c400a69c95
Jared Ostmeyer <jostmey@gmail.com>
2017-04-12 10:42:38 -0500

    Different decay rates for different units

--- run_test.py
+++ run_test.py
@@ -49,7 +49,8 @@ y = tf.placeholder(tf.float32, [batch_size, num_classes])
 
 # Variables
 #
-cell = RWACell(num_cells, decay=[0.5]*62+[0.5**0.1]*63+[0.5**0.01]*62+[1.0]*63)	# Modified code to run RWA model
+decay = [0.5]*62+[0.5**0.1]*63+[0.5**0.01]*62+[1.0]*63	# Create different decay rates for different units
+cell = RWACell(num_cells, decay=decay)	# Modified code to run RWA model
 W_end = tf.Variable(tf.random_normal([num_cells, num_classes], stddev=np.sqrt(1.0/num_cells)))
 b_end = tf.Variable(tf.zeros([num_classes]))
 
